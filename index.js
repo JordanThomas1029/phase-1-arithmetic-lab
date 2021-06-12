7a43b9d7f1fb4504e81707211d0c947a8df13661
Jordan Thomas <tjordan706@gmail.com>
2021-06-12 02:10:59 -0400

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 48;
+let num4 = 12;
+const numbers = 20;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = 8 * 45;
+
+const mod = num3 / num4;
+
+const max = Math.max(numbers);
